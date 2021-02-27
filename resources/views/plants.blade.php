3c42f6fc0b5f4196d2b590ae7a508a311c1e065e
Olgierd <o.dziminski@gmail.com>
2021-02-27 08:04:10 +0100

    Add modal window before deleting a plant

--- resources/views/plants.blade.php
+++ resources/views/plants.blade.php
@@ -38,7 +38,29 @@
             @endif
             <a href="{{ route('displayEditPlant',['id' => $plant->id]) }}">Edit the plant</a><br />
 
-            <a href="{{ route('deletePlant',['id' => $plant->id]) }}">Delete the plant</a><br />
+            <a href="#" data-toggle="modal" data-target="#exampleModal">Delete the plant</a><br />
+            
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Are you sure you wanna delete <strong> {{$plant->name}}? </strong>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+                    <form action="{{ route('deletePlant',['id' => $plant->id]) }}">
+                      <button type="submit" class="btn btn-danger">Yes</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
 
   </div>
 </div>
