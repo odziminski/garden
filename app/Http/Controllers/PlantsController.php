426524bd889d985f8aae3b65690b99ba6d97eb09
odziminski <o.dziminski@gmail.com>
2022-06-23 09:21:13 +0200

    fix: code readability

--- app/Http/Controllers/PlantsController.php
+++ app/Http/Controllers/PlantsController.php
@@ -13,7 +13,7 @@ class PlantsController extends Controller
 {
     protected $dates = ['created_at'];
 
-   
+
 
     public function getRandomPlant()
     {
@@ -34,31 +34,28 @@ class PlantsController extends Controller
                 'nextFertilizing' => $nextFertilizing,
                 // 'trefleData' => $trefleData,
             ]);
-
         } else {
             return view('welcome');
         }
-
-
     }
 
     public function displaySinglePlant($id)
     {
-        $plant = Plant::with('history','needs')
-        ->get()
-        ->where('id', $id)
-        ->first();
+        $plant = Plant::with('history', 'needs')
+            ->get()
+            ->where('id', $id)
+            ->first();
 
         if ($plant->user_id == auth()->id()) {
             $nextWatering = Plant::getNextCareDate($plant->history->watered_at, $plant->needs->watering_frequency);
             $lateForWatering = Carbon::parse($nextWatering)
-                ->diffInDays(Carbon::now(),false);
+                ->diffInDays(Carbon::now(), false);
             $nextWatering = $nextWatering->format('l, j-m-Y ');
 
             $nextFertilizing = Plant::getNextCareDate($plant->history->fertilized_at, $plant->needs->fertilizing_frequency);
 
             $lateForFertilizing = Carbon::parse($nextFertilizing)
-                ->diffInDays(Carbon::now(),false);
+                ->diffInDays(Carbon::now(), false);
             $nextFertilizing = $nextFertilizing->format('l, j-m-Y ');
 
 
@@ -87,7 +84,6 @@ class PlantsController extends Controller
             } else {
                 $uploadedFileUrl = asset("images/plant.png");
             }
-
         }
         try {
             $now = Carbon::now();
@@ -97,21 +93,19 @@ class PlantsController extends Controller
                 'name' => $request->input('name'),
                 'species' => $request->input('species'),
                 'created_at' => $now,
-                
-            ]);
-                $history = History::create([
-                    'plant_id' => $plant->id,
-                     'watered_at' => $now,
-                     'fertilized_at' => $now
-                ]);
 
-                $needs = Needs::create([
-                    'plant_id' => $plant->id,
-                    'watering_frequency' => $request->input('watering_frequency'),
-                    'fertilizing_frequency' => $request->input('fertilizing_frequency')
-                ]);
-            
-            
+            ]);
+            $history = History::create([
+                'plant_id' => $plant->id,
+                'watered_at' => $now,
+                'fertilized_at' => $now
+            ]);
+
+            $needs = Needs::create([
+                'plant_id' => $plant->id,
+                'watering_frequency' => $request->input('watering_frequency'),
+                'fertilizing_frequency' => $request->input('fertilizing_frequency')
+            ]);
         } catch (\Exception $e) {
             $err = $e->getPrevious()->getMessage();
             echo $err;
@@ -127,37 +121,37 @@ class PlantsController extends Controller
 
     public function displayPlants()
     {
-        $plants = Plant::with('history','needs')
-        ->get()
-        ->where('user_id', auth()->id())
-        ->sortByDesc('history.watered_at');
+        $plants = Plant::with('history', 'needs')
+            ->get()
+            ->where('user_id', auth()->id())
+            ->sortByDesc('history.watered_at');
 
         foreach ($plants as $plant) {
-            if(isset($plant->history->watered_at)){
+            if (isset($plant->history->watered_at)) {
                 $plant->watered_at = Plant::getDateForHumans($plant->history->watered_at);
             }
-            if(isset($plant->history->fertilized_at)){
+            if (isset($plant->history->fertilized_at)) {
                 $plant->fertilized_at = Plant::getDateForHumans($plant->history->fertilized_at);
             }
-            if (isset($plant->needs->need_watering)){
+            if (isset($plant->needs->need_watering)) {
                 $plant->need_watering = $plant->needs->need_watering;
             }
-            if (isset($plant->needs->need_fertilizing)){
+            if (isset($plant->needs->need_fertilizing)) {
                 $plant->need_fertilizing = $plant->needs->need_fertilizing;
             }
         }
-        return view('browse')->with('plants',$plants);
+        return view('browse')->with('plants', $plants);
     }
 
-    
+
 
     public function displayEditPlant($id)
     {
-        $plant = Plant::with('history','needs')
-        ->get()
-        ->where('id', $id)
-        ->first();
-        
+        $plant = Plant::with('history', 'needs')
+            ->get()
+            ->where('id', $id)
+            ->first();
+
         return view('edit-plant')->with('plant', $plant);
     }
 
@@ -191,9 +185,9 @@ class PlantsController extends Controller
     public function deletePlant($id)
     {
         Plant::find($id)->delete();
-        Needs::where('plant_id',$id)->delete();
-        History::where('plant_id',$id)->delete();
-        
+        Needs::where('plant_id', $id)->delete();
+        History::where('plant_id', $id)->delete();
+
         return redirect()->route('browse');
     }
 
@@ -203,10 +197,10 @@ class PlantsController extends Controller
         $token = env('TREFLE_TOKEN');
 
         //$response = Http::get('https://trefle.io/api/v1/species/search?token='.$token.'&q='.$species)->json();
-//        if ($response) {
-//            return $data = $response['data'][0];
-//        } else {
-//            return $data = [];
-//        }
+        //        if ($response) {
+        //            return $data = $response['data'][0];
+        //        } else {
+        //            return $data = [];
+        //        }
     }
 }
--- resources/views/edit-plant.blade.php
+++ resources/views/edit-plant.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="add_plant_text_center">
-        <form method="POST" enctype="multipart/form-data" action="{{ route('editPlant',['id' => $plant->id]) }}">
-            @method('PATCH')
-            @csrf
-            @if ($errors->any())
-                <div class="text-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
+<div class="add_plant_text_center">
+       <form method="POST" enctype="multipart/form-data" action="{{ route('editPlant',['id' => $plant->id]) }}">
+              @method('PATCH')
+              @csrf
+              @if ($errors->any())
+              <div class="text-danger">
+                     <ul>
+                            @foreach ($errors->all() as $error)
                             {{ $error }}
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <h4>Name of your plant</h4>
-            <input type="text" class="form-control" name="name" id="name" value="{{ $plant->name }}"> <br/>
-            <h4>What's the species of your plant?</h4>
-            <input type="text" class="form-control" name="species" value="{{ $plant->species }}"> <br/>
-            <h4>Watering frequency required?</h4>
-            <div class="text-left">
-            <input type="number" class="form-control" name="watering_frequency" value="{{ $plant->needs->watering_frequency }}">
+                            @endforeach
+                     </ul>
+              </div>
+              @endif
+              <h4>Name of your plant</h4>
+              <input type="text" class="form-control" name="name" id="name" value="{{ $plant->name }}"> <br />
+              <h4>What's the species of your plant?</h4>
+              <input type="text" class="form-control" name="species" value="{{ $plant->species }}"> <br />
+              <h4>Watering frequency required?</h4>
+              <div class="text-left">
+                     <input type="number" class="form-control" name="watering_frequency" value="{{ $plant->needs->watering_frequency }}">
 
-            </div>
+              </div>
 
-            <h4>Fertilizing frequency required?</h4>
-            <input type="number" class="form-control" name="fertilizing_frequency" value="{{ $plant->needs->fertilizing_frequency }}">
+              <h4>Fertilizing frequency required?</h4>
+              <input type="number" class="form-control" name="fertilizing_frequency" value="{{ $plant->needs->fertilizing_frequency }}">
 
-            <h4>Picture of your plant </h4>
-            <img src="{{str_ireplace( 'https://', 'http://', $plant->avatar )}}"/> <br />
-            <input type="file" name="avatar" accept="image/png, image/jpeg"> <br/> <br/>
-            <input type="submit" value="Add"> <br/>
+              <h4>Picture of your plant </h4>
+              <img src="{{str_ireplace( 'https://', 'http://', $plant->avatar )}}" /> <br />
+              <input type="file" name="avatar" accept="image/png, image/jpeg"> <br /> <br />
+              <input type="submit" value="Add"> <br />
 
-        </form>
-    </div>
-    </div>
+       </form>
+</div>
+</div>
 @endsection
