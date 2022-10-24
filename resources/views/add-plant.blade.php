37bf83fe283649166906b982e13e8c3909b9242e
odziminski <o.dziminski@gmail.com>
2022-10-24 18:13:42 +0200

    fix: remove script import that caused bootstrap.js to not work

--- resources/views/add-plant.blade.php
+++ resources/views/add-plant.blade.php
@@ -3,8 +3,7 @@
 @section('content')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-
+    
 
 
     <div class="container mt-5">
--- resources/views/plants.blade.php
+++ resources/views/plants.blade.php
@@ -4,7 +4,6 @@
 @section('content')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
     <script type="text/javascript" charset="utf-8">
